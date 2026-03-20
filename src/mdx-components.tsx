63169bd7b1e7a36aa9805dae46c0da92d8467287
Heitor Tessaro <heitor@writechoice.io>
2026-03-20 08:45:54 -0300

    docs: add webhooks

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,10 +2,12 @@ import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 import { APIPage } from '@/components/api-page';
 import { Mermaid } from '@/components/mdx/mermaid';
+import { ImageZoom } from 'fumadocs-ui/components/image-zoom';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    img: (props) => <ImageZoom {...(props as any)} />,
     APIPage,
     Mermaid,
     ...components,
